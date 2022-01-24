51b5c914b4748b6c478dd19b2378f3b5f912c1f8
Manish Sencha <manishsencha2001@gmail.com>
2022-01-24 20:02:31 +0530

    completed stopwatch

--- src/app/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stopwatch',
   templateUrl: './stopwatch.component.html',
-  styleUrls: ['./stopwatch.component.css']
+  styleUrls: ['./stopwatch.component.css'],
 })
 export class StopwatchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  private interval: any;
+  private time = 0;
+  public hh: any = 0;
+  public mm: any = 0;
+  public ss: any = 0;
+  public ms: any = 0;
+  public started = false;
+  public start() {
+    this.interval = setInterval(() => {
+      this.started = true;
+      this.time += 1;
+      let x = this.time;
+      this.hh = Math.floor(x / 360000);
+      x %= 360000;
+      this.mm = Math.floor(x / 6000);
+      x %= 6000;
+      this.ss = Math.floor(x / 100);
+      x %= 100;
+      this.ms = x;
+    }, 10);
+  }
+  public stop() {
+    clearInterval(this.interval);
+    this.started = false;
+  }
+  public reset() {
+    clearInterval(this.interval);
+    this.hh = 0;
+    this.mm = 0;
+    this.ss = 0;
+    this.ms = 0;
+    this.time = 0;
+    this.started = false;
   }
 
+  ngOnInit(): void {}
 }
